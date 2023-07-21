59d8c96424d2a70b5bada53a72f8d81b8651aa05
Phunguy65 <55185818+Phunguy65@users.noreply.github.com>
2023-07-21 05:39:57 -0700

    Delete Problem_06 directory

--- Problem_06/generator.cpp
+++
@@ -1,9 +0,0 @@
-#include "rndlib.h"
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    srand(time(NULL));
-    auto a = Random::integer(1,10000);
-    auto b = Random::integer(1,10000);
-    cout << a <<" "<<b;
-}
