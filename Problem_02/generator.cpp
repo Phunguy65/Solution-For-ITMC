4185efdba5acf4f7fb5241655107ae7d7a5cf8cb
Phunguy65 <55185818+Phunguy65@users.noreply.github.com>
2023-07-21 05:38:36 -0700

    Delete Problem_02 directory

--- Problem_02/generator.cpp
+++
@@ -1,8 +0,0 @@
-#include "rndlib.h"
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    srand(time(NULL));
-    auto x = Random::String(100000,"abcdfghjuytreq");
-    cout <<x<<endl;
-}
